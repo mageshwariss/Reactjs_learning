0a6333a8624a4a61e746dfaa6ae9833b02057657
Mageshwari <mageshwaris221@gmail.com>
2025-05-11 04:44:59 +0530

    modified es6

--- es6/src/Arrowwithpara.js
+++ es6/src/Arrowwithpara.js
@@ -1,16 +1,3 @@
-//import React from 'react';
-//import ReactDOM from 'react-dom';
-   /*  var additionwithpara = (x) =>
-    {
-      var a = 5;
-      if(a===5)
-      {
-      //  let a = 10;
-      }
-      document.getElementById('arrowwithpara').innerHTML ="arrowwithpara "+ a;
-    }
-//additionwithpara(5);
-export default additionwithpara; */
 export const additionwithpara=(n)=>
  <div>arrowwithpara {n} + 5 = {n + 5}</div>;
 
--- es6/src/Es6file.js
+++ es6/src/Es6file.js
@@ -1,25 +1,3 @@
-//import React from 'react';
-//import ReactDOM from 'react-dom';
-/* class Demo {
-  addition() {
-    var a = 15;
-    var b = 10;
-    document.getElementById('es6add').innerHTML = "ES6 add=" + a + b;
-  }
-  subtraction() {
-    var a = 5;
-    var b = 10;
-    var c = a - b;
-    document.getElementById('es6sub').innerHTML = c;
-  }
-}
-//var mydemo = new Demo();
-//mydemo.addition();
-//mydemo.subtraction();
-
-export default Demo;
- */
-
 class Demo {
   addition() {
     return <div>es6add 10 + 20 = {10 + 20}</div>;
