23528e93bdeb88534ef3c58c627b7ee1be81bfa2
Mageshwari <mageshwaris221@gmail.com>
2025-05-11 11:07:43 +0530

    Add react-jsx folder

---
+++ react-jsx/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const mystyle = {
+  color: 'blue'
+};
+
+const myele = (
+  <div style={mystyle}>
+    <h1>Welcome</h1>
+    <h2>Hello World</h2>
+  </div>
+);
+
+// ✅ createRoot and render
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(myele);
